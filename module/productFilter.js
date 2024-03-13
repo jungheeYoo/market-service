467741a9df30f877a1dfe266dfaa73cab3249643
Joy <jungheeyoo.joy@gmail.com>
2024-03-14 00:24:48 +0900

    리스트 페이지 필터 2

--- module/productFilter.js
+++ module/productFilter.js
@@ -9,14 +9,42 @@ const discountFilter = document.getElementById('discount-filter');
 const filterButton =
   document.getElementsByClassName('product-filter-con')[0]?.lastElementChild;
 
+const convertPriceToNumber = (originalPrice) => {
+  // 입력 창을 클릭했을 때
+  // event.target.value => 3,000원
+  const formattedString = String(originalPrice)
+    .replace('원', '')
+    .replace(',', '');
+  const formattedNumber = Number(formattedString);
+  return isNaN(formattedNumber) ? 0 : formattedNumber;
+};
+
+const formatToPrice = (event) => {
+  // 입력창을 벗어날 때
+  // 입력된 값이 format되어 표시
+  const value = event.target.value;
+  const result = Number(value);
+  if (isNaN(result)) {
+    alert('숫자를 입력해주세요.');
+  }
+
+  event.target.value = `${result.toLocaleString()}원`;
+};
+
+const convertPercentToNumber = (originalValue) => {
+  const formattedString = String(originalValue).replace('%', '');
+  const formattedNumber = Number(formattedString);
+  return isNaN(formattedNumber) ? 0 : formattedNumber;
+};
+
 // 만약 필터 버튼 누르면 -> min, max, discount 값을 받아옴
 // -> 값을 이용하여 해당하는 물품을 추출 -> 다시 화면에 나타냄
 
 export const setButtonEvent = (productList) => {
   filterButton.onclick = () => {
-    const maxPrice = maxPriceFilter.value || MAX_PRICE;
-    const minPrice = minPriceFilter.value || 0;
-    const discountRate = discountFilter.value || 0;
+    const maxPrice = convertPriceToNumber(maxPriceFilter.value) || MAX_PRICE;
+    const minPrice = convertPriceToNumber(minPriceFilter.value) || 0;
+    const discountRate = convertPercentToNumber(discountFilter.value) || 0;
     // console.log(maxPrice, minPrice, discountPrice);
     const newProductList = productList.filter((productInfo) => {
       const { price, discountPercent } = productInfo;
@@ -43,5 +71,39 @@ export const setButtonEvent = (productList) => {
       });
       sectionDOM.appendChild(emptyProductListDOM);
     }
+  };
+};
+
+export const setFilterEvent = () => {
+  // 필터 DOM들이 이벤트 핸들러를 구현
+  // 필터 입력창 -> 사용자가 클릭 -> 숫자
+  // 필터 입력창 이외의 부분 -> 사용자가 클릭 ->
+  // 입력창에서 벗어남 -> '원', '%'가 붙은 format된 형태
+
+  minPriceFilter.onfocus = (event) => {
+    event.target.value = convertPriceToNumber(event.target.value);
+  };
+  minPriceFilter.onblur = formatToPrice;
+  maxPriceFilter.onfocus = (event) => {
+    event.target.value = convertPriceToNumber(event.target.value);
+  };
+  maxPriceFilter.onblur = formatToPrice;
+  discountFilter.onfocus = (event) => {
+    event.target.value = convertPercentToNumber(event.target.value);
+  };
+  discountFilter.onblur = (event) => {
+    const value = event.target.value;
+    const result = Number(value);
+    if (isNaN(result)) {
+      alert('숫자를 입력해주세요.');
+      event.target.value = 0;
+      return;
+    }
+    if (result > 100 || result < 0) {
+      alert('0이상 100이하의 숫자를 입력해주세요.');
+      event.target.value = 0;
+      return;
+    }
+    event.target.value = `${result}%`;
   };
 };
